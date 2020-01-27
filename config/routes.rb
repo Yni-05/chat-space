090c78bc1774814fd632d205c2af7846bbb82538
Yni-05 <yhforwork1@gamil.com>
2020-01-27 14:11:42 +0900

    group-create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "messages#index"
+  root "groups#index"
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+    #index: 投稿されたメッセージの一覧表示 & メッセージの入力ができる
+    #create: メッセージの保存を行う
+  end
 end
