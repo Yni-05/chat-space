cc74b96a0c53cc116b88e2f388923879a45c4037
Yni-05 <yhforwork1@gamil.com>
2020-01-31 07:39:03 +0900

    fix-css

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,13 +3,13 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
-    @message = @group.messages.includes(:user)
+    @messages = @group.messages.includes(:user)
   end
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_message_path(@group), notice: 'メッセージが送信されました'
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       flash.now[:alert] = 'メッセージを入力してください'
       render :index
